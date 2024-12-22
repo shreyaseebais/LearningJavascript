6fbf30698f035980bc284e6f484ccb07ecc3ecfe
shreyasee <shreyasee.bais@gmail.com>
2024-12-22 10:30:11 +0530

    nested array

--- 2_nestedArray.js
+++ 2_nestedArray.js
@@ -1,20 +1,34 @@
-console.log('#####Nested Array######');
+/////     NESTED ARRAY  ///////
 const nestedArray = [[2,3],[2,6,7,9],87,12,[23,[56,22,[45,66,78]]]];
 
 //Method1 
-const result1 = nestedArray.flat(Infinity);
-console.log(result);
-//Method2
-const result2 = flattenTheArray(nestedArray);
-console.log(result);
+var result = nestedArray.flat(Infinity);
+console.log("Using Array.flat() : ", result);
 
-function flattenTheArray(nestedArray){
+
+//Method2
+result = flattenTheArray(nestedArray);
+console.log("Using for loop: ", result);
+
+function flattenTheArray(arr){
     let flatArray = [];
-    for(let i=0;i<nestedArray.length;i++){
-        if(Array.isArray(nestedArray[i])){
-            flatArray = flatArray.concat(flattenTheArray(nestedArray[i]))
+    for(let i=0;i<arr.length;i++){
+        if(Array.isArray(arr[i])){
+            flatArray = flatArray.concat(flattenTheArray(arr[i]))
         }else{
-            flatArray.push(nestedArray[i]);
+            flatArray.push(arr[i]);
         }
     }
+    return flatArray;
+}
+
+
+//Method 3 Reduce method
+result = flattenTheArrayUsingReduce(nestedArray);
+console.log("Using reduce method: ", result);
+
+function flattenTheArrayUsingReduce(arr){
+    return arr.reduce((acc, item)=>{
+        return acc.concat(Array.isArray(item)? flattenTheArrayUsingReduce(item):item)
+    }, []); 
 }
